ea52b3040051d8589438716d54a7d4f256d41fb1
Jcpaul1226 <155691679+Jcpaul1226@users.noreply.github.com>
2024-10-02 16:11:46 -0700

    Add files via upload

---
+++ token/token.go
@@ -0,0 +1,52 @@
+// First step in creating our interpreter will be creating a Lexer that will turn our source code into tokens
+// This file will hold all of our tokens that our lexer will be assigning to our source code input
+
+package token
+
+type TokenType string //string allows us to use many different types of tokens
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
