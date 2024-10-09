4755659d0adcddf064701e903265b2a32d19430b
Jcpaul1226 <155691679+Jcpaul1226@users.noreply.github.com>
2024-10-09 16:17:34 -0700

    Add files via upload

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
@@ -11,16 +12,24 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	//read two tokens, so curToken and peekToken are both set
 	p.NextToken()
 	p.NextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
 }
 
 func (p *Parser) NextToken() {
@@ -47,6 +56,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -65,12 +76,24 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	//TODO: we're skippinmg the expressions untill we encounter a semicolon
+	//TODO: we're skipping the expressions untill we encounter a semicolon
 
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.NextToken()
 	}
 
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.NextToken()
+
+	//We're skipping the expressions untill we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.NextToken()
+	}
 	return stmt
 }
 
@@ -87,6 +110,12 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.NextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
